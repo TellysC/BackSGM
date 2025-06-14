de25b958024557c5057d810fd51715cf089cf4a4
Tellys Cauã <tellyscaua704@gmail.com>
2025-06-13 21:30:30 -0300

    corrigindo bugs do crud

--- src/main/java/com/exemple/maintenance_system_api/domain/model/Endereco.java
+++ src/main/java/com/exemple/maintenance_system_api/domain/model/Endereco.java
@@ -22,13 +22,19 @@ public class Endereco {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    @Column(nullable = false)
     private String logradouro;
+    @Column(nullable = false)
     private int numero;
     @Column(nullable = false)
     private String bairro;
+    @Column(nullable = false)
     private String cidade;
+    @Column(nullable = false)
     private String estado;
+    @Column(nullable = false)
     private String cep;
+    @Column(nullable = false)
     private String pais;
 
     @CreationTimestamp
--- src/main/java/com/exemple/maintenance_system_api/domain/model/dto/ContatoCreateDTO.java
+++ src/main/java/com/exemple/maintenance_system_api/domain/model/dto/ContatoCreateDTO.java
@@ -1,4 +1,4 @@
 package com.exemple.maintenance_system_api.domain.model.dto;
 
-public record ContatoCreateDTO (String telefone, String celular,Long fkCodigoDistanciaId, CodigoDistanciaCreateDTO codigoDistanciaCreateDTO) {
+public record ContatoCreateDTO (String telefone, String celular, CodigoDistanciaCreateDTO codigoDistanciaCreateDTO) {
 }
