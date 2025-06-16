a2549c3160e95da9af4317d6d219d2e4478e6cd1
PedroCorrea7 <pedrohfcorrea@icloud.com>
2025-06-16 03:43:15 -0300

    Criando Exceptions

--- src/main/java/com/exemple/maintenance_system_api/domain/model/dto/CodigoDistanciaUpdateDTO.java
+++ src/main/java/com/exemple/maintenance_system_api/domain/model/dto/CodigoDistanciaUpdateDTO.java
@@ -1,4 +1,12 @@
 package com.exemple.maintenance_system_api.domain.model.dto;
 
 public record CodigoDistanciaUpdateDTO(String numero, String estado) {
+    public CodigoDistanciaUpdateDTO {
+        if (numero == null || numero.isBlank()) {
+            throw new IllegalArgumentException("Número não pode ser nulo ou em branco");
+        }
+        if (estado == null || estado.isBlank()) {
+            throw new IllegalArgumentException("Estado não pode ser nulo ou em branco");
+        }
+    }
 }
